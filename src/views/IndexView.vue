463a78f7fd0e519cac5a446d5285981e3b5c50fb
Matias Sallent <mqsallent@gmail.com>
2022-07-02 20:43:45 -0300

    Render list of pokemon

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+import { usePokemon } from '@/hooks/usePokemon';
+const { pokemon, isLoading, error } = usePokemon();
+</script>
+
 <template>
   <div>PokeGuessr</div>
+
+  <div v-if="isLoading">Loading...</div>
+
+  <div v-if="error">{{ error.message }}</div>
+
+  <ul v-if="!isLoading && !error">
+    <li v-for="(_pokemon, index) in pokemon" :key="_pokemon.name">
+      {{ `${_pokemon.name} (id: ${index + 1})` }}
+    </li>
+  </ul>
 </template>
