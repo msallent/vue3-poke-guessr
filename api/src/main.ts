290f3ef9857429ed40e15eaf90fad0a68f97ff5d
Matias Sallent <mqsallent@gmail.com>
2022-07-04 23:49:01 -0300

    Add Swagger support

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: { origin: 'http://localhost:3030' },
   });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('PokeGuessr API')
+    .setDescription('PokeGuessr API Documentation')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
 
   await app.listen(8080);
 }
