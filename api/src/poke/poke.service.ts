5bb846959112d5c3869ad7f0a8fb7fdf1ac83902
Matias Sallent <mqsallent@gmail.com>
2022-07-05 21:16:12 -0300

    Add limit and offset to findAll service method

--- api/src/poke/poke.service.ts
+++ api/src/poke/poke.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { map } from 'rxjs';
-import { Pokemon, PokemonList } from './interfaces/poke.interface';
+import { Pokemon, PokemonList } from './interfaces';
 
 @Injectable()
 export class PokeService {
   constructor(private readonly httpService: HttpService) {}
 
-  findAll() {
-    return this.httpService.get<PokemonList>('/pokemon').pipe(map(({ data }) => data));
+  findAll(limit = 20, offset = 0) {
+    return this.httpService
+      .get<PokemonList>(`/pokemon?limit=${limit}&offset=${offset}`)
+      .pipe(map(({ data }) => data));
   }
 
   findOne(id: number) {
