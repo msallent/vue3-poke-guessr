75b9ffd7cdd6b405bd94191ac16e2f5d5c32bd20
Matias Sallent <mqsallent@gmail.com>
2022-07-06 19:05:12 -0300

    Add text-white when active

--- client/src/components/CardButton.vue
+++ client/src/components/CardButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     type="button"
-    class="rounded-md p-2.5 w-full shadow-md font-bold capitalize border border-white bg-white text-black hover:border-black hover:bg-black hover:text-white active:border-gray-900 active:bg-gray-900 active:shadow-none"
+    class="rounded-md p-2.5 w-full shadow-md font-bold capitalize border border-white bg-white text-black hover:border-black hover:bg-black hover:text-white active:border-gray-900 active:bg-gray-900 active:text-white active:shadow-none"
   >
     <slot></slot>
   </button>
