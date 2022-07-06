a270f24e8ca3588deb931c7e0884fc1d0e8e0cb8
Matias Sallent <mqsallent@gmail.com>
2022-07-05 21:16:31 -0300

    Add route that selects 4 random pokemon for the quiz

--- api/src/poke/poke.controller.ts
+++ api/src/poke/poke.controller.ts
@@ -1,13 +1,38 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 import { PokeService } from './poke.service';
+import { MAX_CARD_OPTIONS, MAX_ID_SECOND_GEN } from './constants';
+import { getRandomNumber } from '@/utils';
 
 @Controller()
 export class PokeController {
   constructor(private readonly pokeService: PokeService) {}
 
   @Get()
-  findAll() {
-    return this.pokeService.findAll();
+  findAll(@Query() query: { limit: number; offset: number }) {
+    return this.pokeService.findAll(query.limit, query.offset);
+  }
+
+  @Get('quiz')
+  async generateQuiz() {
+    const limit = 100;
+
+    let randomOffset = getRandomNumber(MAX_ID_SECOND_GEN) - limit;
+    randomOffset = randomOffset < 0 ? 0 : randomOffset;
+
+    const pokemonList = await firstValueFrom(this.findAll({ limit, offset: randomOffset }));
+    const randomPokemonIndices = [...Array(MAX_CARD_OPTIONS)].map(() => getRandomNumber(limit));
+
+    return randomPokemonIndices.map((index) => {
+      const pokemon = pokemonList.results[index];
+      const pokemonId = pokemon.url.split('/pokemon/')[1].split('/')[0];
+
+      return {
+        id: pokemonId,
+        name: pokemon.name,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`,
+      };
+    });
   }
 
   @Get(':id')
