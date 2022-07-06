f569c953257b5ac16c33e7371f6052c570fd2bfc
Matias Sallent <mqsallent@gmail.com>
2022-07-06 19:04:59 -0300

    Move usePokemon to CardButton

--- client/src/views/IndexView.vue
+++ client/src/views/IndexView.vue
@@ -1,18 +1,10 @@
 <script setup lang="ts">
 import PokemonCard from '@/components/PokemonCard.vue';
-import { usePokemon } from '@/hooks/usePokemon';
-
-const { pokemonList, pokemon, isLoading, error } = usePokemon();
 </script>
 
 <template>
-  <div>PokeGuessr</div>
-
-  <div v-if="isLoading">Loading...</div>
-
-  <div v-else-if="error">{{ error.message }}</div>
-
-  <div v-else-if="pokemon" class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-    <PokemonCard :pokemon="pokemon" :options="pokemonList" />
+  <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <h1 class="text-4xl mb-4 text-center text-[#ffcb05]">PokeGuessr</h1>
+    <PokemonCard />
   </div>
 </template>
