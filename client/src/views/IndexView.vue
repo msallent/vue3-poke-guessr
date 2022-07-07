78a6f5e9a237bb7fc89caf926e9053af0e15153a
Matias Sallent <mqsallent@gmail.com>
2022-07-06 21:57:21 -0300

    Add text-shadow

--- client/src/views/IndexView.vue
+++ client/src/views/IndexView.vue
@@ -4,7 +4,9 @@ import PokemonCard from '@/components/PokemonCard.vue';
 
 <template>
   <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-    <h1 class="text-4xl mb-4 text-center text-[#ffcb05]">PokeGuessr</h1>
+    <h1 class="text-4xl mb-4 text-center text-[#ffcb05] [text-shadow:1px_2px_#2a75bb]">
+      PokeGuessr
+    </h1>
     <PokemonCard />
   </div>
 </template>
