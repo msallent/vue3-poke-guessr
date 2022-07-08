5025f3965273e1287cf1c328c69b367b4a09546e
Matias Sallent <mqsallent@gmail.com>
2022-07-08 13:57:06 -0300

    Fix score bar edges

--- client/src/components/ScoreBar.vue
+++ client/src/components/ScoreBar.vue
@@ -11,11 +11,17 @@ defineProps<{
     <div class="w-full flex mx-4 overflow-hidden -skew-x-[25deg] gap-2.5">
       <div
         class="bg-green-600 skew-x-[45deg] transition-all"
-        :style="{ width: `${(correctChoices * 100) / (correctChoices + incorrectChoices)}%` }"
+        :style="{
+          width: `${(correctChoices * 100) / (correctChoices + incorrectChoices)}%`,
+          display: correctChoices > 0 ? 'block' : 'none',
+        }"
       ></div>
       <div
         class="bg-red-600 skew-x-[45deg] transition-all"
-        :style="{ width: `${(incorrectChoices * 100) / (correctChoices + incorrectChoices)}%` }"
+        :style="{
+          width: `${(incorrectChoices * 100) / (correctChoices + incorrectChoices)}%`,
+          display: incorrectChoices > 0 ? 'block' : 'none',
+        }"
       ></div>
     </div>
     <span class="font-bold text-red-600">{{ incorrectChoices }}</span>
